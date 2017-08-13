fc9d0078b53a7b25d29531298d3de4c804b83d9d
crazyqoder <30309118+crazyqoder@users.noreply.github.com>
2017-08-13 20:26:27 +0530

    Create numericchar.c

---
+++ numericchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+   char line[100];
+   int i,count=0,len;
+   gets(line);
+   len=strlen(line);
+   for(i=0;i<len;i++)
+   {
+    if((line[i]>='0')&&(line[i]<='9'))
+    count++;
+   }
+    printf("%d",count);
+}
