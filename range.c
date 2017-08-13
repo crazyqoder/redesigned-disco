c6559d5bf680e986e321e2d39cda37d71e44f6cf
crazyqoder <30309118+crazyqoder@users.noreply.github.com>
2017-08-13 21:42:56 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{   int num;
+    printf("enter a num between 1 to 9");
+    scanf("%d",&num);
+    switch(num)
+    {
+       case 1 :  
+             break;
+       case 2 : 
+          break;
+       case 3 :
+           break;
+      case  4 :
+          break;
+       case 5 :
+          break;
+       case 6 :
+          break;
+       case 7 :
+           break;
+       case 8 :
+          break;
+       case 9 :
+           break;
+        default:
+          printf("you entered number is not in range");
+         
+    }
+}
