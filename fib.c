18d5235394ec8868c76c1a52ca2412a884590aaa
crazyqoder <30309118+crazyqoder@users.noreply.github.com>
2017-08-13 17:45:22 +0530

    Create fib.c

---
+++ fib.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+   int i, n, t1 = 0, t2 = 1, nextTerm;
+
+    scanf("%d",&n);
+    //printf("%d\t%d\t",f1,f2);
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    
+}
